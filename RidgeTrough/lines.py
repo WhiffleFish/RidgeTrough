011cd27ef2aecac42e6fb3c715bb080af8b36543
WhiffleFish <tylertronics@gmail.com>
2023-06-14 07:50:39 -0600

    meeting changes

--- RidgeTrough/lines.py
+++ RidgeTrough/lines.py
@@ -5,6 +5,7 @@ from netCDF4 import Dataset
 from tqdm import tqdm
 import numpy as np
 import datetime
+from scipy import signal
 import re
 import os
 import gc
@@ -12,7 +13,7 @@ import gc
 def grads(hgts, sigma=(5,3), verbose=False, gc_freq=1000):
     grad_tensor = np.zeros(hgts.shape)
     for day in tqdm(range(hgts.shape[0]), desc='getting gradients'):
-        if day % gc_freq == 0: gc.collect()
+        if day % gc_freq == 0: gc.collect() # temporary memory leak solution?
         for level in range(hgts.shape[1]):
             tmp_grad = ndimage.gaussian_gradient_magnitude(hgts[day, level,...], sigma=sigma)
             grad_tensor[day, level] = tmp_grad
@@ -70,32 +71,38 @@ def year_range(hgt_path):
     return min_year, max_year
 
 def retrieve_data(path):
-        fh = Dataset(path, mode='r')
-        lons = fh.variables['lon'][:].data
-        lats = fh.variables['lat'][:].data
-        times = fh.variables['time'][:].data
-        times = times - times[0]
-        data = fh.variables['hgt'][:].data
-        time = datetime.datetime.now().strftime('%H:%M:%S')
-        
-        return (times, lats, lons, data)
+    '''
+    get data from a single year
+    '''
+    fh = Dataset(path, mode='r')
+    lons = fh.variables['lon'][:].data
+    lats = fh.variables['lat'][:].data
+    times = fh.variables['time'][:].data
+    times = times - times[0]
+    data = fh.variables['hgt'][:].data
+    time = datetime.datetime.now().strftime('%H:%M:%S')
+    
+    return (times, lats, lons, data)
 
 def get_data(hgt_path, yr_range = None, verbose=True):
-        if yr_range is None: yr_range = year_range(hgt_path)
-        years = []
-        times = []
-        yearly_data = []
-        lats, lons = None, None
+    '''
+    get data from multiple years
+    '''
+    if yr_range is None: yr_range = year_range(hgt_path)
+    years = []
+    times = []
+    yearly_data = []
+    lats, lons = None, None
 
-        for year in tqdm(range(yr_range[0], yr_range[1]+1), desc='retrieving data'):
-            t, _lats, _lons, data = retrieve_data(os.path.join(hgt_path, f'hgt.{year}.nc'))
-            if lats is None: lats = _lats
-            if lons is None: lons = _lons
-            years.append(year)
-            times.append(t)
-            yearly_data.append(data)
-        
-        return years, times, lats, lons, yearly_data
+    for year in tqdm(range(yr_range[0], yr_range[1]+1), desc='retrieving data'):
+        t, _lats, _lons, data = retrieve_data(os.path.join(hgt_path, f'hgt.{year}.nc'))
+        if lats is None: lats = _lats
+        if lons is None: lons = _lons
+        years.append(year)
+        times.append(t)
+        yearly_data.append(data)
+    
+    return years, times, lats, lons, yearly_data
 
 
 class FullCDFData():
@@ -116,6 +123,58 @@ class FullCDFData():
         self.grad_lines = max_grads(self.grads, sigma=sigma)
 
 
-class PeakFinder():
-    def __init__(self):
-        pass
+class CritPointFinder():
+    def __init__(self, prominence=1.5, distance=4):
+        self.prominence = prominence
+        self.distance = distance
+
+    def __call__(self, data): # convenience method
+        return self.find(data)
+    
+    def find(self, data):
+        # [day, level, lat]
+        if isinstance(data, FullCDFData):
+            return self._find_3d(data.grad_lines)
+        elif len(data.shape) == 3:
+            return self._find_3d(data)
+        elif len(data.shape) == 2:
+            return self._find_2d(data)
+        elif len(data.shape) == 1:
+            return self._find_1d(data)
+        else:
+            assert False
+    
+    def _find_1d(self, data):
+        assert len(data.shape) == 1
+        troughs = signal.find_peaks(-data, distance=self.distance, prominence=self.prominence)[0]
+        peaks = signal.find_peaks(data, distance=self.distance, prominence=self.prominence)[0]
+        return peaks, troughs
+
+    def _find_2d(self, data):
+        assert len(data.shape) == 2
+        peaks = []
+        troughs = []
+        for i in range(data.shape[0]):
+            _peaks, _troughs = self._find_1d(data[i,:])
+            peaks.append(_peaks)
+            troughs.append(_troughs)
+        
+        return peaks, troughs
+    
+    def _find_3d(self, data):
+        peaks = []
+        troughs = []
+        for i in range(data.shape[0]):
+            _peaks, _troughs = self._find_2d(data[i,:,:])
+            peaks.append(_peaks)
+            troughs.append(_troughs)
+        
+        return peaks, troughs
+
+def plot_crit_points(peaks, troughs, data, day, level):
+    fig, ax = plt.subplots()
+    ax.contourf(data.hgts[day,level,...], cmap='YlOrRd', levels=10)
+    ax.scatter(troughs[day][level], data.grad_lines[day, level][troughs[day][level]], marker='x',s=100)
+    ax.scatter(peaks[day][level], data.grad_lines[day, level][peaks[day][level]], marker='x', s=100,color='purple')
+    ax.axis('off')
+    return fig, ax
